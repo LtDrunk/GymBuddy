b73f08680f2c3f536769a0f70df593255e1de6b4
tobnil1 <tobias.nilsson@intunio.se>
2018-11-04 20:10:31 +0100

    Added five assistance exercises

--- app/src/main/java/se/tobias/gymbuddy/AssistanceExercisesContainer.java
+++ app/src/main/java/se/tobias/gymbuddy/AssistanceExercisesContainer.java
@@ -8,6 +8,10 @@ import android.widget.LinearLayout;
 public class AssistanceExercisesContainer extends LinearLayout {
     private Context _context;
     private AssistanceExercise firstAssistanceExercise = null;
+    private AssistanceExercise secondAssistanceExercise = null;
+    private AssistanceExercise thirdAssistanceExercise = null;
+    private AssistanceExercise fourthAssistanceExercise = null;
+    private AssistanceExercise fifthAssistanceExercise = null;
 
     public AssistanceExercisesContainer(Context context, @Nullable AttributeSet attrs){
         super(context, attrs);
@@ -21,8 +25,24 @@ public class AssistanceExercisesContainer extends LinearLayout {
         inflate(_context, R.layout.assistance_exercises_container, this);
 
         firstAssistanceExercise = findViewById(R.id.firstAssistanceExercise);
-        firstAssistanceExercise.setExerciseHeader("Sidoplanka");
-        firstAssistanceExercise.setExerciseSets("10 sekunder");
+        firstAssistanceExercise.setExerciseHeader("Gymnast Abs");
+        firstAssistanceExercise.setExerciseSets("10 lyft");
+
+        secondAssistanceExercise = findViewById(R.id.secondAssistanceExercise);
+        secondAssistanceExercise.setExerciseHeader("Sidoplanka med höjd arm");
+        secondAssistanceExercise.setExerciseSets("10 lyft");
+
+        thirdAssistanceExercise = findViewById(R.id.thirdAssistanceExercise);
+        thirdAssistanceExercise.setExerciseHeader("Luta framåt med vikt");
+        thirdAssistanceExercise.setExerciseSets("1 X 10 @ 10kg");
+
+        fourthAssistanceExercise = findViewById(R.id.fourthAssistanceExercise);
+        fourthAssistanceExercise.setExerciseHeader("Bicep curl");
+        fourthAssistanceExercise.setExerciseSets("1 X 10 @ 10kg");
+
+        fifthAssistanceExercise = findViewById(R.id.fifthAssistanceExercise);
+        fifthAssistanceExercise.setExerciseHeader("Inclined dumbell bench press");
+        fifthAssistanceExercise.setExerciseSets("1 X 10 @ 10kg");
 
     }
 
